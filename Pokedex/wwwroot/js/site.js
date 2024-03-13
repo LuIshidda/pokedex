360bffcd17c7706d90facf304c5a72cdba68cf91
LuIshidda <luizhenrisansou23@gmail.com>
2024-03-12 21:26:40 -0300

    Tentado Finalizar

--- Pokedex/wwwroot/js/site.js
+++ Pokedex/wwwroot/js/site.js
@@ -12,7 +12,7 @@
     };
     for (i = 0; i < buttons.length; i++) {
         if (buttons[i].id == 'btn-${type}') {
-            buttons[i].classList.remove("btn-sn");
+            buttons[i].classList.remove("btn-sm");
             buttons[i].classList.add("btn-md");
         }
         else {
